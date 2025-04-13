a4a9a1dcef74cb95c6672382a8f365dec91ef291
Muhammad Abdullah <Abdullahtrq2@gmail.com>
2025-04-13 21:15:59 +0500

    Update admin navigation: Rename 'View Users' link to 'Users' for improved clarity and consistency in the user interface.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
                             <div x-show="open" @click.away="open = false" class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10">
                                 @if(Auth::user()->isAdmin())
                                     <a href="{{ route('admin.users.index') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                                        <i class="fas fa-users mr-2 text-green-500"></i>View Users
+                                        <i class="fas fa-users mr-2 text-green-500"></i>Users
                                     </a>
                                 @endif
                                 
