cf8d3bab6734b0fb589b35684074827d9768444a
Muhammad Abdullah <Abdullahtrq2@gmail.com>
2025-04-13 17:37:21 +0500

    Remove success message display from technicians index view to streamline the user interface.

--- resources/views/technicians/index.blade.php
+++ resources/views/technicians/index.blade.php
@@ -13,12 +13,6 @@
         </a>
     </div>
 
-    @if(session('success'))
-        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
-            <span class="block sm:inline">{{ session('success') }}</span>
-        </div>
-    @endif
-
     <div class="bg-white shadow-md rounded-lg overflow-hidden">
         <table class="min-w-full leading-normal">
             <thead>
