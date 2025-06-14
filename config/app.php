5041a43785e34d7560b7fd212b16fbefc01db7cf
Muhammad Abdullah <Abdullahtrq2@gmail.com>
2025-06-14 21:14:58 +0500

    refactor: update service provider configuration in app.php
    - Reorganized the service provider array in config/app.php to explicitly list Laravel Framework Service Providers, enhancing clarity and maintainability.
    - Removed the previous merging method to streamline the configuration structure, ensuring better readability and adherence to Laravel conventions.

--- config/app.php
+++ config/app.php
@@ -133,10 +133,32 @@ return [
     | this array to grant expanded functionality to your applications.
     |
     */
-    'providers' => ServiceProvider::defaultProviders()->merge([
+    'providers' => [
         /*
-         * Package Service Providers...
+         * Laravel Framework Service Providers...
          */
+        Illuminate\Auth\AuthServiceProvider::class,
+        Illuminate\Broadcasting\BroadcastServiceProvider::class,
+        Illuminate\Bus\BusServiceProvider::class,
+        Illuminate\Cache\CacheServiceProvider::class,
+        Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
+        Illuminate\Cookie\CookieServiceProvider::class,
+        Illuminate\Database\DatabaseServiceProvider::class,
+        Illuminate\Encryption\EncryptionServiceProvider::class,
+        Illuminate\Filesystem\FilesystemServiceProvider::class,
+        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
+        Illuminate\Hashing\HashServiceProvider::class,
+        Illuminate\Mail\MailServiceProvider::class,
+        Illuminate\Notifications\NotificationServiceProvider::class,
+        Illuminate\Pagination\PaginationServiceProvider::class,
+        Illuminate\Pipeline\PipelineServiceProvider::class,
+        Illuminate\Queue\QueueServiceProvider::class,
+        Illuminate\Redis\RedisServiceProvider::class,
+        Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
+        Illuminate\Session\SessionServiceProvider::class,
+        Illuminate\Translation\TranslationServiceProvider::class,
+        Illuminate\Validation\ValidationServiceProvider::class,
+        Illuminate\View\ViewServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -146,6 +168,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\Smtp2goServiceProvider::class,
-    ])->toArray(),
+    ],
 
 ];
