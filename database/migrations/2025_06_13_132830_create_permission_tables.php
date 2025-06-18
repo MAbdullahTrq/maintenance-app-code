3bd622aab79ce512af94597837a51d97eb1b5a0f
Muhammad Abdullah <Abdullahtrq2@gmail.com>
2025-06-19 02:38:58 +0500

    fix migrations

--- database/migrations/2025_06_13_132830_create_permission_tables.php
+++ database/migrations/2025_06_13_132830_create_permission_tables.php
@@ -20,6 +20,11 @@ return new class extends Migration
         throw_if(empty($tableNames), new Exception('Error: config/permission.php not loaded. Run [php artisan config:clear] and try again.'));
         throw_if($teams && empty($columnNames['team_foreign_key'] ?? null), new Exception('Error: team_foreign_key on config/permission.php not loaded. Run [php artisan config:clear] and try again.'));
 
+        // Drop tables if they exist from failed migrations
+        Schema::dropIfExists($tableNames['role_has_permissions']);
+        Schema::dropIfExists($tableNames['model_has_roles']);
+        Schema::dropIfExists($tableNames['model_has_permissions']);
+
         // Comment out permissions table creation
         // Schema::create($tableNames['permissions'], static function (Blueprint $table) {
         //     $table->bigIncrements('id');
